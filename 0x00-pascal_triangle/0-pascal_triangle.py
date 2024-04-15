2a57eaedcf47cc3b99e0714ce8bda153b9b0807a
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-04-15 12:21:24 +0100

    Pascal triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A function that returns a list of lists of integers representing
+the Pascal's triangle of n
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the
+    Pascal's triangle of n. It returns an empty list if
+    n <= 0. n will always be an integer.
+    """
+    if n <= 0:
+        return ([])
+    
+    result = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(result[i - 1][j - 1] + result[i - 1][j])
+        row.append(1)
+        result.append(row)
+
+    return (result)
