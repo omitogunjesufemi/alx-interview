57e7b17f75bde0ff66e126fceabb026e8c7f6432
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-08-10 11:04:49 +0100

    Code refactoring

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -19,6 +19,7 @@ def isWinner(x, nums):
     Return: name of the player that won the most rounds
     If the winner cannot be determined, return `None`
     """
+    """
     if len(nums) <= 0 or x <= 0:
         return None
 
@@ -54,3 +55,37 @@ def isWinner(x, nums):
         return "Maria"
 
     return None
+    """
+
+    if x <= 0 or len(nums) == 0:
+        return None
+
+    max_n = max(nums)
+
+    sieve = [True] * (max_n + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_n + 1, i):
+                sieve[j] = False
+
+    primes_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        primes_count[i] = primes_count[i - 1] + (1 if sieve[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if primes_count[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
